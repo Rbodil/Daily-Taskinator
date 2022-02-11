c9b1fc42db4fe4ea7df7aab718ac7fa4bb33adf8
Reagan Bodily <reaganbodily@gmail.com>
2022-02-11 11:41:00 -0700

    form reset, refactored code

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,23 +1,44 @@
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-var formEl = document.querySelector("#task-form")
+var formEl = document.querySelector("#task-form");
 
 
-function createTaskHandler(event) {
+function taskFormHandler(event) {
     event.preventDefault();
     
     var newTask = document.querySelector("input[name='task-name']").value;
     var newtType = document.querySelector("select[name = 'task-type']").value;
-    console.dir(newtType)
 
+    if(!newTask || !newtType) {
+        alert("You must fill out the form");
+        return false;
+    }
+    formEl.reset();
+
+    var taskDataObj = {
+        name: newTask,
+        type: newtType
+    };
+
+    createTaskEl(taskDataObj);
+    
+};
+
+
+function createTaskEl(taskDataObj){
+    
     var listItemEl = document.createElement('li');
     listItemEl.className = "task-item";
 
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + newTask + "</h3><span class='task-type'>" + newtType + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     
 
     tasksToDoEl.appendChild(listItemEl);
     listItemEl.appendChild(taskInfoEl);
-}
-formEl.addEventListener('submit', createTaskHandler);
+    
+};
+
+
+
+formEl.addEventListener('submit', taskFormHandler);
